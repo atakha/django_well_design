e4fd7c774223deecd36bdeefe2490f9b049ad4b6
atakha <atakhacp@gmail.com>
2023-03-05 07:14:32 +0600

    Initial setup config core and User

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
+# Register your models here.
